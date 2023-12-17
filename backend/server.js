3f278dc410bffef5af25dd5cdf2e9b30abbcdcdd
MATT6007 <MATT6007@users.noreply.github.com>
2023-12-18 00:20:54 +0100

    Test comment res file

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,12 @@ app.use(express.json());
 
 app.use('/genealogy', require('./src/routers/genealogyRouter'));
 
-app.use(express.static(path.join(__dirname, 'frontend/build', 'index.html')));
-if(process.env.NODE_ENV === 'production')
-{ 
-    app.use(express.static(path.join(__dirname, 'frontend/build', 'index.html')));
-    app.get('*', (req, res) => {res.sendFile(path.join(__dirname = 'frontend/build/index.html'));  })
-}
+// app.use(express.static(path.join(__dirname, 'frontend/build', 'index.html')));
+// if(process.env.NODE_ENV === 'production')
+// { 
+//     app.use(express.static(path.join(__dirname, 'frontend/build', 'index.html')));
+//     app.get('*', (req, res) => {res.sendFile(path.join(__dirname = 'frontend/build/index.html'));  })
+// }
 
 
 app.listen(port, () => {
